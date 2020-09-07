2c587f351031b38a3df84e0707e5a698731461c2
gevorgepamworks <gevorgitmkrtchyan@gmail.com>
2020-09-07 22:31:27 +0400

    adding generic service

---
+++ src/main/java/com/example/crud_mvc/services/CrudService.java
@@ -0,0 +1,13 @@
+package com.example.crud_mvc.services;
+
+import java.util.List;
+
+public interface CrudService<T, ID> {
+    List<T> findAll();
+
+    T findById(ID id);
+
+    T save(T object);
+
+    void deleteById(ID id);
+}
