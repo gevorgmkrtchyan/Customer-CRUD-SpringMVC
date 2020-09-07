5270b770d55174361c65c687f432d348d2a07249
gevorgepamworks <gevorgitmkrtchyan@gmail.com>
2020-09-07 22:13:30 +0400

    adding starter project

---
+++ src/test/java/com/example/crud_mvc/CustomerCrudMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crud_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerCrudMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
