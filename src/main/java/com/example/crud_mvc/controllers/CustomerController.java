0cd508a7002e1622827ea0729cac843215f5196d
gevorgepamworks <gevorgitmkrtchyan@gmail.com>
2020-09-09 22:23:29 +0400

    implementing delete function in controller

--- src/main/java/com/example/crud_mvc/controllers/CustomerController.java
+++ src/main/java/com/example/crud_mvc/controllers/CustomerController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -33,6 +34,11 @@ public class CustomerController {
     @PostMapping("saveCustomer")
     public String saveCostumer(@ModelAttribute("customer") Customer customer){
         customerService.save(customer);
+        return "redirect:/list";
+    }
+    @PostMapping("deleteCustomer/{id}")
+    public String deleteCustomer(@PathVariable int id){
+        customerService.deleteById(id);
         return "redirect:/list";
     }
 }
