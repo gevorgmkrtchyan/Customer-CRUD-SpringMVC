778ef7de86d3c3fde81851a4cf148b73363d4bf9
gevorgepamworks <gevorgitmkrtchyan@gmail.com>
2020-09-09 21:06:12 +0400

    adding list form and controller method

--- src/main/java/com/example/crud_mvc/controllers/CustomerController.java
+++ src/main/java/com/example/crud_mvc/controllers/CustomerController.java
@@ -1,7 +1,12 @@
 package com.example.crud_mvc.controllers;
 
+import com.example.crud_mvc.model.Customer;
 import com.example.crud_mvc.services.CustomerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class CustomerController {
@@ -10,5 +15,12 @@ public class CustomerController {
 
     public CustomerController(CustomerService customerService) {
         this.customerService = customerService;
+    }
+
+    @GetMapping("/list")
+    public String showCustomers(Model model) {
+        List<Customer> customers = customerService.findAll();
+        model.addAttribute("customers", customers);
+        return "customer-list";
     }
 }
--- src/main/java/com/example/crud_mvc/model/Customer.java
+++ src/main/java/com/example/crud_mvc/model/Customer.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "customers")
+@Table(name = "customer")
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
